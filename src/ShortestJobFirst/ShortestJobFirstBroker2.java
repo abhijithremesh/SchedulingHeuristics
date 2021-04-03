3a744813ad8082e8981e481e51372715e472fb49
abhijithremesh <abhijithremesh@gmail.com>
2021-04-03 23:04:19 +0200

    update

---
+++ src/ShortestJobFirst/ShortestJobFirstBroker2.java
@@ -0,0 +1,75 @@
+package org.cloudsimplus.examples;
+
+import org.cloudbus.cloudsim.brokers.DatacenterBrokerSimple;
+import org.cloudbus.cloudsim.cloudlets.Cloudlet;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.vms.Vm;
+
+import java.util.List;
+
+public class ShortestJobFirstBroker2 extends DatacenterBrokerSimple {
+
+    public ShortestJobFirstBroker2(final CloudSim simulation) {
+        super(simulation);
+    }
+
+    public void scheduleTasksToVms(List<Vm> vmList, List<Cloudlet> cloudletList ) {
+
+        // Printing cloudlets before sorting
+        for (Cloudlet c : cloudletList)
+        {
+            System.out.println(" Cloudlet "+c.getId()+" length: "+c.getLength());
+        }
+
+        // Sorting cloudlets in ascending order with respect to its length
+        cloudletList.sort((Cloudlet c1,Cloudlet c2) -> {
+            if (c1.getLength() > c2.getLength())
+                return 1;
+            if (c1.getLength() < c2.getLength())
+                return -1;
+            return 0;
+        });
+
+        System.out.println("******************************************* ");
+
+        // Printing cloudlets after sorting
+        for (Cloudlet c : cloudletList)
+        {
+            System.out.println(" Cloudlet "+c.getId()+" length: "+c.getLength());
+        }
+
+        System.out.println("******************************************* ");
+
+        // Binding the sorted cloudlets to the VMs as per the First Come First Serve Policy
+        for(int i=0;i<cloudletList.size();i++){
+            Cloudlet cl = cloudletList.get(i);
+            Vm vm = vmList.get((i % vmList.size()));
+            bindCloudletToVm(cl,vm);
+            System.out.println("Cloudlet "+cl.getId()+" is bound with VM "+vm.getId());
+        }
+
+    }
+
+    public List<Cloudlet> OrderShortestJobFirst(List<Cloudlet> cloudletList){
+
+        System.out.println("\n\tShortest Job First Broker Schedules\n");
+
+        cloudletList.sort((Cloudlet c1,Cloudlet c2) -> {
+            if (c1.getLength() > c2.getLength())
+                return 1;
+            if (c1.getLength() < c2.getLength())
+                return -1;
+            return 0;
+        });
+        System.out.println("Sorting the cloudlets....");
+        for(int i=0;i<cloudletList.size();i++)
+        {
+            System.out.println("Inside broker");
+            System.out.println("Index "+i+" holds cloudlet "+cloudletList.get(i).getId()+" having length "+cloudletList.get(i).getLength());
+        }
+
+        return cloudletList;
+
+    }
+
+}
